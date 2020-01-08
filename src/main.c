7cc7fee813664ea418107b5cbecdf3d731df5eea
arnauochoa <arnau.ochoa@gmail.com>
2020-01-08 22:30:55 +0100

    Genetic Algorithm skeleton done

--- src/main.c
+++ src/main.c
@@ -3,15 +3,41 @@
 
 #include "build_image_matrix.h"
 #include "write_image_matrix.h"
+#include "genetic_algorithm.h"
 #include <stdint.h>
 
+
+Image segmentateImage(Image image);
 
 int main() {
     char *path = "../resources/testBW_small.png";
 
+    // get design parameters from console and check values (percentages and a, b)
+
     Image image = buildImage(path);
 
-    writeImage(image);
+    Image segImage = segmentateImage(image);
+
+    writeImage(segImage);
 
     return EXIT_SUCCESS;
+}
+
+Image segmentateImage(Image image) {
+    DesignParameters designParameters; // TODO: initialize
+
+    int *population = initializePopulation(image, designParameters);
+
+    int hasConverged = 0;
+    int newVariance;
+    int oldVariance = INT32_MAX;
+
+    while (!hasConverged) {
+        population = evolvePopulation(population, designParameters);
+        hasConverged = testConvergence(image, population, designParameters, oldVariance, &newVariance);
+        oldVariance = newVariance;
+    }
+
+
+    return image;
 }
